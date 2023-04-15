50f2e68dec341f3a0ed7d43e01cf7c688905b865
EarlHikky <andrew.malofeev@gmail.com>
2023-04-15 19:31:40 +0300

    get_average_salary added

--- main.py
+++ main.py
@@ -1,50 +1,41 @@
+from collections import defaultdict
 from pprint import pprint
-
 import requests
 
-url = 'https://api.hh.ru/vacancies/'
-languages = [
-    'JavaScript', 'Java', 'Python', 'Ruby',
-    'PHP', 'C++', 'C#', 'C', 'Go', 'Scala'
-]
 
-area = 1
-period = 30
-professional_role = 96
-
-# text = 'Python'
-# professional_roles = {'id': 96}, {'name': ''}
-# professional_role = "Программист, разработчик"
-# params = {'search_field': 'name', 'text': text, 'area': area, 'period': period}
-# params = {'professional_roles': 96}
-params = {'professional_role': professional_role, 'area': area, 'period': period}
-
-
-def get_vacancies_for_lang():
-    vacancies = dict()
-    for language in languages:
-        params.update(text=language)
-        response = requests.get(url, params)
-        response.raise_for_status()
-        vacancies.setdefault(language, response.json()['found'])
-    print(vacancies)
-
-
-def get_vacancies_for_python(language):
+def get_response(url, params, language):
     params.update(text=language)
     response = requests.get(url, params)
     response.raise_for_status()
-    for vacancy in response.json()['items']:
-        if vacancy['salary']:
-            print(predict_rub_salary(vacancy['salary']))
+    return response.json()
+
+
+def get_vacancies(url, params, languages):
+    vacancies_stats = defaultdict(dict)
+    for language in languages:
+        vacancies = get_response(url, params, language)
+        average_salary, vacancies_processed = get_average_salary(vacancies)
+        vacancies_stats[language].setdefault('vacancies_found', vacancies['found'])
+        vacancies_stats[language].setdefault('average_salary', average_salary)
+        vacancies_stats[language].setdefault('vacancies_processed', vacancies_processed)
+    return dict(vacancies_stats)
+
+
+def get_average_salary(vacancies):
+    average_salary = []
+    vacancies_processed = 0
+    for vacancy in vacancies['items']:
+        if not vacancy['salary'] or vacancy['salary']['currency'] != 'RUR':
+            continue
+        average_salary.append(predict_rub_salary(vacancy['salary']))
+        vacancies_processed += 1
+    return sum(average_salary), vacancies_processed
 
 
 def predict_rub_salary(vacancy):
     salary_from = vacancy['from']
     salary_to = vacancy['to']
-    if not vacancy['currency'] == 'RUR':
-        return
-    elif salary_from and salary_to:
+    if salary_from and salary_to:
         predicted_salary = (salary_from + salary_to) / 2
     elif salary_from:
         predicted_salary = salary_from * 1.2
@@ -52,9 +43,21 @@ def predict_rub_salary(vacancy):
         predicted_salary = salary_to * 0.8
     else:
         return
-    return predicted_salary
+    return int(predicted_salary)
 
 
-# pprint(response.json()) {'from': 100000, 'to': 180000, 'currency': 'RUR', 'gross': False}
-# get_vacancies_for_python()
-get_vacancies_for_python('python')
+def main():
+    url = 'https://api.hh.ru/vacancies/'
+    area = 1
+    period = 30
+    professional_role = 96
+    params = {'professional_role': professional_role, 'area': area, 'period': period}
+    languages = [
+        'JavaScript', 'Java', 'Python', 'Ruby',
+        'PHP', 'C++', 'C#', 'C', 'Go', 'Scala',
+    ]
+    pprint(get_vacancies(url, params, languages))
+
+
+if __name__ == '__main__':
+    main()
