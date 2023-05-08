40ad0809ed315f61480ed7b493496451bb9ecf93
EarlHikky <andrew.malofeev@gmail.com>
2023-05-09 00:27:39 +0300

    переменная окружения вынесена в "if main"

--- get_stats.py
+++ get_stats.py
@@ -1,16 +1,103 @@
 import os
+import requests
 from collections import defaultdict
 from terminaltables import DoubleTable
 from dotenv import load_dotenv
-
-from get_stats_utils import get_average_salary
-from hh_api import get_hh_vacancies
-from sj_api import get_sj_vacancies
+from itertools import count
 
 LANGUAGES = [
     'JavaScript', 'Java', 'Python', 'Ruby',
     'PHP', 'C++', 'C#', 'C', 'Go', 'Scala',
 ]
+
+AREA = 1  # Moscow
+PERIOD = 30  # Days
+PROFESSIONAL_ROLE = 96  # 'Programmer, developer'
+PER_PAGE = 100
+HH_API_URL = 'https://api.hh.ru/vacancies/'
+
+TOWN = 4  # Moscow
+PROFESSION = 48  # 'Programmer, developer'
+SJ_API_URL = 'https://api.superjob.ru/2.0/vacancies/'
+
+
+def predict_rub_salary(vacancy):
+    """Returns average salary for a vacancy in RUB"""
+    salary_from = vacancy.get('payment_from') or vacancy.get('salary', dict()).get('from')
+    salary_to = vacancy.get('payment_to') or vacancy.get('salary', dict()).get('to')
+    if salary_from and salary_to:
+        predicted_salary = (salary_from + salary_to) / 2
+    elif salary_from:
+        predicted_salary = salary_from * 1.2
+    elif salary_to:
+        predicted_salary = salary_to * 0.8
+    else:
+        return 0, 0
+    return predicted_salary, 1
+
+
+def currency_check(vacancy):
+    """Checking a type of currency"""
+    if not vacancy.get('salary'):
+        return vacancy.get('currency') == 'rub'
+    else:
+        return vacancy['salary']['currency'] == 'RUR'
+
+
+def get_average_salary(vacancies):
+    """Get average salary for a language in RUB"""
+    average_salaries = []
+    vacancies_processed = 0
+    for vacancy in vacancies:
+        if not currency_check(vacancy):
+            continue
+        salary, counter = predict_rub_salary(vacancy)
+        average_salaries.append(salary)
+        vacancies_processed += counter
+    try:
+        return int(sum(average_salaries) / len(average_salaries)), vacancies_processed
+    except ZeroDivisionError:
+        return 0, 0
+
+
+def get_response(url, headers, params):
+    """Get a response from a service"""
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    raw_vacancies = response.json()
+    vacancies = raw_vacancies.get('items') or raw_vacancies.get('objects')
+    vacancies_found = raw_vacancies.get('total') or raw_vacancies.get('found') or 0
+    available_vacancies_check = raw_vacancies.get('pages') or raw_vacancies.get('more')
+    return vacancies, available_vacancies_check, vacancies_found
+
+
+def get_sj_vacancies(language):
+    """Get all vacancies for a language from the SuperJob"""
+    headers = {'X-Api-App-Id': sj_api_key}
+    vacancies_roster = []
+    params = {'catalogues': PROFESSION, 'keyword': language, 'town': TOWN,
+              'count': PER_PAGE}
+    for page in count(0):
+        params['page'] = page
+        vacancies, available_vacancies_check, vacancies_found = get_response(SJ_API_URL, headers, params)
+        vacancies_roster.extend(vacancies)
+        if not available_vacancies_check:
+            break
+    return vacancies_roster, vacancies_found
+
+
+def get_hh_vacancies(language):
+    """Get all vacancies for a language from the HeadHunter"""
+    params = {'professional_role': PROFESSIONAL_ROLE, 'area': AREA, 'text': language, 'period': PERIOD,
+              'per_page': PER_PAGE}
+    vacancies_roster = []
+    for page in count(0):
+        params['page'] = page
+        vacancies, available_vacancies_check, vacancies_found = get_response(HH_API_URL, {}, params)
+        vacancies_roster.extend(vacancies)
+        if page >= available_vacancies_check - 1:
+            break
+    return vacancies_roster, vacancies_found
 
 
 def get_vacancies_stats(get_vacancies):
@@ -26,10 +113,6 @@ def get_vacancies_stats(get_vacancies):
 
 
 def main():
-    env_path = os.path.join(os.path.dirname(__file__), '.env')
-    if not os.path.exists(env_path):
-        raise FileNotFoundError('.env does not exist')
-    load_dotenv(env_path)
     hh_vacancies_stats = get_vacancies_stats(get_hh_vacancies)
     sj_vacancies_stats = get_vacancies_stats(get_sj_vacancies)
     for index, stats in enumerate((hh_vacancies_stats, sj_vacancies_stats)):
@@ -42,4 +125,9 @@ def main():
 
 
 if __name__ == '__main__':
+    env_path = os.path.join(os.path.dirname(__file__), '.env')
+    if not os.path.exists(env_path):
+        raise FileNotFoundError('.env does not exist')
+    load_dotenv(env_path)
+    sj_api_key = os.environ.get('SJ_SECRET_KEY')
     main()
